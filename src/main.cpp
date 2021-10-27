512ccbed943e461a8869b6767ef51e240c643067
andrew <ajfarquharson1@gmail.com>
2021-10-27 15:01:29 +0100

    Basic motor functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <Arduino.h>
+#include <SparkFun_TB6612.h>
+
+const int offsetA = 1;
+const int offsetB = 1;
+
+#define AIN1 2
+#define BIN1 7
+#define AIN2 4
+#define BIN2 6
+#define PWMA 3
+#define PWMB 5
+#define STBY 9
+
+Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
+Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
 
 void setup() {
-  // put your setup code here, to run once:
+  motor1.drive(255,1000);
 }
 
 void loop() {
