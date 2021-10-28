4b99ff69a36a2a512e8edf3eb527e4b85334e4b3
andrew <ajfarquharson1@gmail.com>
2021-10-28 19:30:38 +0100

    1st motion

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,25 @@
 const int offsetA = 1;
 const int offsetB = 1;
 
-#define AIN1 2
-#define BIN1 7
-#define AIN2 4
-#define BIN2 6
-#define PWMA 3
-#define PWMB 5
-#define STBY 9
+#define AIN1 7
+#define BIN1 5
+#define AIN2 8
+#define BIN2 4
+#define PWMA 9
+#define PWMB 3
+#define STBY 6
 
 Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
 Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
 
+
+
 void setup() {
-  motor1.drive(255,1000);
+  forward(motor1, motor2, 255);
+  delay(2000);
+  right(motor1, motor2, 255);
+  delay(2000);
+  forward(motor1, motor2, 255);
 }
 
 void loop() {
